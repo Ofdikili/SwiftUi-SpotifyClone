c6c75fcdfceeb3a983a9774bfefba864abd2686e
Ofdikili <omerfarukdikili00@gmail.com>
2024-04-21 15:48:50 +0300

    Initial Commit

---
+++ SwiftUi-SpotifyClone/SwiftUi_SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_SpotifyCloneApp.swift
+//  SwiftUi-SpotifyClone
+//
+//  Created by Ömer Faruk Dikili on 21.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
